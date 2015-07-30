ee3e8762b4ac1c7a8d83b5c9bb65414dd237bcd4
SteveLeeLX <steveleelx@gmail.com>
2015-07-30 16:00:02 +0800

    add media dir

--- CourseMaster/settings.py
+++ CourseMaster/settings.py
@@ -106,8 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     ('css', os.path.join(STATIC_ROOT, 'css').replace('\\', '/')),
