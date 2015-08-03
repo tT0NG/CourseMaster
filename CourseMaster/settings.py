a231056581f8c64a5fd31a3380e4b7c224d83539
SteveLeeLX <steveleelx@gmail.com>
2015-08-04 01:30:32 +0800

    secure settings and add 404 page

--- CourseMaster/settings.py
+++ CourseMaster/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r@fp^q^z%-0s*h=&szgrw6ve9^2%d+($q&a2m9&@ijflhnd&qr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['classgotcha.com']
 
 
 # Application definition
@@ -123,3 +123,11 @@ STATICFILES_DIRS = (
 
 AUTH_USER_MODEL = 'Account.Account'
 LOGIN_REDIRECT_URL = '/index'
+
+# Secure settings
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
